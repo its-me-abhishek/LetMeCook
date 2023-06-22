95802254b0359da7ee72206d50f14540d147f15f
its-me-abhishek <bumblebee31304@gmail.com>
2023-06-22 22:11:52 +0530

    Added features in about section

--- app.py
+++ app.py
@@ -12,7 +12,17 @@ def home():
 #About Page
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    git_ak = 'https://github.com/its-me-abhishek'
+    git_as = 'https://github.com/abhijitshukla'
+    li_ak = 'https://www.linkedin.com/in/abhishek-%E2%80%8E-6009a224b/'
+    li_as = 'https://www.linkedin.com/in/abhijit-kumar-shukla-b41739272/'
+    li_ar = 'https://www.linkedin.com/in/aditya-raina-65aa66257'
+    li_at = 'https://www.linkedin.com/in/jerryyyy'
+    em_ak = 'abhishek31304@gmail.com'
+    em_as = 'abhijitshukla888@gmail.com'
+    em_ar = 'adityaraina204@gmail.com'
+    em_at = 'akshrivastav8864@gmail.com'
+    return render_template('about.html', git_ak=git_ak, git_as=git_as, li_ak=li_ak, li_as=li_as, li_ar=li_ar, li_at=li_at, em_ak=em_ak, em_as=em_as, em_ar=em_ar, em_at=em_at)
 
 #Github
 @app.route('/github', methods=['GET'])
